300df2826d256cc5021dc61c5a4bb2926f587bad
Ashutosh Kumar Singh <ashu012002@gmail.com>
2023-02-27 10:18:35 +0530

    Added solution - LeetHub

---
+++ Odd or Even - GFG/odd-or-even.cpp
@@ -0,0 +1,30 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h> 
+using namespace std; 
+
+// } Driver Code Ends
+class Solution{   
+public:
+    string oddEven(int N){
+        // code here 
+        string a="odd", b="even";
+        if(N&1)return a;
+        else return b;
+    }
+};
+
+//{ Driver Code Starts.
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int N;
+        cin >> N;
+        Solution ob;
+        cout << ob.oddEven(N) << endl;
+    }
+    return 0; 
+} 
+// } Driver Code Ends
