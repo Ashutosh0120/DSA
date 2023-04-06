c873607b84ec7feb11cc7c9e2f91736f6aaf21b8
Ashutosh Kumar Singh <ashu012002@gmail.com>
2023-04-06 21:34:27 +0530

    Added solution - LeetHub

--- Path With Minimum Effort - GFG/path-with-minimum-effort.cpp
+++ Path With Minimum Effort - GFG/path-with-minimum-effort.cpp
@@ -3,39 +3,36 @@
 using namespace std;
 
 // } Driver Code Ends
-
 #define pii pair<int,pair<int,int>>
 class Solution {
   public:
     int MinimumEffort(vector<vector<int>>& h) {
-        priority_queue<pii,vector<pii>,greater<pii>> p; 
-        int n=h.size(), m=h[0].size();
-        
-        vector<vector<int>> d(n,vector<int>(m,1e9));
-        d[0][0]=0;
-        p.push({0,{0,0}});
-        int dr[]={0,1,0,-1},dc[]={1,0,-1,0};
-        while(!p.empty()){
-            auto x=p.top();
-            p.pop();
-            int di=x.first;
-            int r=x.second.first, c=x.second.second;
-            if(r==n-1 and c==m-1){
-                return di;
-            }
+        priority_queue<pii,vector<pii>,greater<pii>> pq;  //priority_queue
+        int n=h.size(), m=h[0].size(); //l,b
+        vector<vector<int>> d(n,vector<int>(m,1e9));//dis
+        // TC= 0(N*M*4*LOG(N*M))
+        d[0][0]=0; //start
+        pq.push({0,{0,0}});  //start pos inside the dis
+        int dr[]={1,0,-1,0}, dc[]={0,1,0,-1};
+        while(!pq.empty()){
+            auto p=pq.top();pq.pop();
+            int w=p.first;
+            int x=p.second.first, y=p.second.second;
+            if(x==n-1 and y==m-1) return w;
             for(int i=0;i<4;i++){
-                int nr=dr[i]+r;
-                int nc=dc[i]+c;
-                if(nr>=0 and nc>=0 and nr<n and nc<m){
-                    int ne=max(abs(h[r][c]-h[nr][nc]),di);
-                    if(ne<(d[nr][nc])){
-                       d[nr][nc]=ne;
-                       p.push({ne,{nr,nc}});
+                int nr=dr[i]+x;
+                int nc=dc[i]+y;
+                if(nr>=0 and nr<n and nc>=0 and nc<m){
+                    int nd=max(w,abs(h[nr][nc]-h[x][y]));
+                    if(nd<d[nr][nc]){
+                        d[nr][nc]=nd;
+                        pq.push({nd,{nr,nc}});
                     }
                 }
             }
         }
         return 0;
+        
     }
 };
 
